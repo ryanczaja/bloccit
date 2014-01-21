2c0e6e21b059611b1732df948ef8ac58d430cc7b
Ryan Czaja <ryanczaja@mac.com>
2014-01-21 22:02:28 +0000

    Adding image to Post

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,8 @@ if Rails.env.development?
 		:address => 'smtp.sendgrid.net',
 		:port => '587',
 		:authentication => :plain,
-		:user_name => "app21281812@heroku.com",
-		:password => "w0xhiepi",
+		:user_name => ENV['SENDGRID_USERNAME'],
+		:password => ENV['SENDGRID_PASSWORD'],
 		:domain => 'heroku.com',
 		:enable_starttls_auto => true
 	}
